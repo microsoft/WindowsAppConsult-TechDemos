e0ecc42344ff782e484b73493c8f0a0b186b252e
Sebastien Bovo <sbovo@microsoft.com>
2021-03-17 16:09:28 +0100

    .NET 5 using WinRT APIs

---
+++ dotNET5/WpfAppWith-NET5/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppWith_NET5
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private async void btnGetGeoLocation_Click(object sender, RoutedEventArgs e)
+		{
+			Windows.Devices.Geolocation.Geolocator gl = new Windows.Devices.Geolocation.Geolocator();
+			Windows.Devices.Geolocation.Geoposition gp = await gl.GetGeopositionAsync();
+			tbGeolocation.Text = $"My location is lat={gp.Coordinate.Latitude}, long={gp.Coordinate.Longitude}";
+		}
+	}
+}
